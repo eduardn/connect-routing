41def2a4acd66395be0bfe86429925226357e45f
Eduard Neculaesi <neculaesi.eduard@gmail.com>
2015-10-25 12:29:08 +0200

    Change tabs to spaces

--- test/e2e/Router.spec.js
+++ test/e2e/Router.spec.js
@@ -10,28 +10,28 @@ app.use(Router.middleware);
 
 // Simple route
 router.addRoute('/test', function (req, res, next) {
-	res.end('I was called');
-	next();
+    res.end('I was called');
+    next();
 });
 
 // Route with one parameter
 router.addRoute('/test/:id', function (req, res, next) {
-	res.end('I was called with param ' + req.params.id);
-	next();
+    res.end('I was called with param ' + req.params.id);
+    next();
 });
 
 describe('Router e2e', function () {
-	it('should call a simple route', function (done) {
-		request(app)
-			.get('/test')
-			.expect(200)
-			.expect('I was called', done);
-	});
+    it('should call a simple route', function (done) {
+        request(app)
+            .get('/test')
+            .expect(200)
+            .expect('I was called', done);
+    });
 
-	it('should call a route with one parameter', function (done) {
-		request(app)
-			.get('/test/10')
-			.expect(200)
-			.expect('I was called with param ' + 10, done);
-	});
+    it('should call a route with one parameter', function (done) {
+        request(app)
+            .get('/test/10')
+            .expect(200)
+            .expect('I was called with param ' + 10, done);
+    });
 });
--- test/unit/Router.spec.js
+++ test/unit/Router.spec.js
@@ -3,70 +3,70 @@ var noop = function() {};
 var router;
 
 describe('Router', function () {
-	beforeEach(function () {
-		router = Router.instance();
-	});
+    beforeEach(function () {
+        router = Router.instance();
+    });
 
-	describe('#middleware()', function () {
-		var requestParams = {
-			req: {
-				url: '/test/route',
-				method: 'GET'
-			},
-			res: {},
-			next: function() {}
-		};
+    describe('#middleware()', function () {
+        var requestParams = {
+            req: {
+                url: '/test/route',
+                method: 'GET'
+            },
+            res: {},
+            next: function() {}
+        };
 
-		it('should call callRoute', function () {
-			spyOn(router, 'callRoute');
-			Router.middleware(requestParams.req, requestParams.res, requestParams.next);
+        it('should call callRoute', function () {
+            spyOn(router, 'callRoute');
+            Router.middleware(requestParams.req, requestParams.res, requestParams.next);
 
-			expect(router.callRoute).toHaveBeenCalled();
-			expect(router.callRoute).toHaveBeenCalledWith(
-				requestParams.req.url, requestParams.req, requestParams.res, requestParams.next);
-		});
+            expect(router.callRoute).toHaveBeenCalled();
+            expect(router.callRoute).toHaveBeenCalledWith(
+                requestParams.req.url, requestParams.req, requestParams.res, requestParams.next);
+        });
 
-		it('should match a simple route', function () {
-			var routeFunction = jasmine.createSpy('routeAction', function () {});
-			router.addRoute('/test/route', routeFunction);
+        it('should match a simple route', function () {
+            var routeFunction = jasmine.createSpy('routeAction', function () {});
+            router.addRoute('/test/route', routeFunction);
 
-			Router.middleware(requestParams.req, requestParams.res, requestParams.next);
+            Router.middleware(requestParams.req, requestParams.res, requestParams.next);
 
-			expect(routeFunction).toHaveBeenCalled();
-			expect(routeFunction).toHaveBeenCalledWith(
-				requestParams.req, requestParams.res, requestParams.next);
-		});
+            expect(routeFunction).toHaveBeenCalled();
+            expect(routeFunction).toHaveBeenCalledWith(
+                requestParams.req, requestParams.res, requestParams.next);
+        });
 
-		it('should match a route with one parameter', function () {
-			var routeFunction = jasmine.createSpy('routeAction', function () {});
-			requestParams.req.url = '/test/route/1';
+        it('should match a route with one parameter', function () {
+            var routeFunction = jasmine.createSpy('routeAction', function () {});
+            requestParams.req.url = '/test/route/1';
 
-			router.addRoute('/test/route/:id', routeFunction);
-			Router.middleware(requestParams.req, requestParams.res, requestParams.next);
+            router.addRoute('/test/route/:id', routeFunction);
+            Router.middleware(requestParams.req, requestParams.res, requestParams.next);
 
-			expect(routeFunction).toHaveBeenCalled();
-			expect(routeFunction.mostRecentCall.args.length).toEqual(3);
-			expect(routeFunction).toHaveBeenCalledWith(
-				requestParams.req, requestParams.res, requestParams.next);
+            expect(routeFunction).toHaveBeenCalled();
+            expect(routeFunction.mostRecentCall.args.length).toEqual(3);
+            expect(routeFunction).toHaveBeenCalledWith(
+                requestParams.req, requestParams.res, requestParams.next);
 
-			// Get the request argument from the route function
-			var req = routeFunction.mostRecentCall.args[0];
+            // Get the request argument from the route function
+            var req = routeFunction.mostRecentCall.args[0];
 
-			expect(req.params).toBeDefined();
-			expect(req.params.id).toEqual('1');
-		});
-	});
+            expect(req.params).toBeDefined();
+            expect(req.params.id).toEqual('1');
+        });
+    });
 
-	describe('#addRoute()', function () {
-		beforeEach(function () {
-			// Reset routes
-			router.routes.length = 0;
-		});
+    describe('#addRoute()', function () {
+        beforeEach(function () {
+            // Reset routes
+            router.routes.length = 0;
+        });
 
-		it('should add a new route to the list', function () {
-			router.addRoute('/test/add/route', noop);
+        it('should add a new route to the list', function () {
+            router.addRoute('/test/add/route', noop);
 
-			expect(router.routes.length).toEqual(1);
-		});
-	});
+            expect(router.routes.length).toEqual(1);
+        });
+    });
 });
